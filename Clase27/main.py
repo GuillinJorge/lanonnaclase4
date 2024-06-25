b63aeeca9de4265ea31a82a55dd3ce5fb527a3ac
GuillinJorge <51805016+GuillinJorge@users.noreply.github.com>
2024-06-25 19:45:47 -0300

    >>ver2

--- Clase27/main.py
+++ Clase27/main.py
@@ -1,7 +1,24 @@
-def main():
-    for i in range(0,10):
-        print (f"Este es el ciclo {i}")
+from modules.get_user import get_user
 
+
+def main():
+    
+    user = get_user()
+    
+    print(user)
+        
+    # user = add_email_pwd(user)
+    
+    
+    # save_user_db(user, option)
+    
+    
+    # user = add_custom_mg(user, custom_mg)
+    
+    
+    # send_email(user, smtp_server)
+    
+    
 if __name__ == "__main__":
     main()
     
