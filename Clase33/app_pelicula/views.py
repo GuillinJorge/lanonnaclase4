2a8211924fb7e65bdc61cbec69bd628ba350391e
GuillinJorge <jorgeguillin@hotmail.com>
2024-07-03 23:14:50 -0300

    >> A Seguir!!!

--- Clase33/app_pelicula/views.py
+++ Clase33/app_pelicula/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.db import models
 
-# Create your views here.
+class Pelicula(models.Model):
+    nombre = models.CharField(max_length=200, unique=True)
+    fecha_de_release = models.DateField()
+    genero = models.CharField(max_length=200)
+    duracion = models.IntegerField()
+    
+    def __str__(self) -> str:
+        return self.nombre
+    
+    class Meta:
+        db_table = 'pelicula'
+
